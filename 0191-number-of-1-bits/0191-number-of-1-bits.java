45dde847d15db62ed2a4faa71747ead6add62288
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-06-30 10:51:26 +0530

    Time: 0 ms (100%), Space: 40.5 MB (85.36%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int hammingWeight(int n) {
+        // int cnt = 0;
+        // while(n>0){
+        //     if(n%2 == 1){
+        //         cnt++;
+        //     }
+        //     n/=2;
+        // }
+        // return cnt;
+
+
+        int cnt = 0;
+        while(n != 0){
+            cnt++;
+            n = n & n - 1;
+        }
+        return cnt;
+    }
+}
