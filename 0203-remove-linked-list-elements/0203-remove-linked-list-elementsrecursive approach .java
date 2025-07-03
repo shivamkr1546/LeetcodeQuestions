927f2c701b603531ed331e3036b530828df9e4bc
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-07-04 00:29:25 +0530

    Time: 1 ms (93.48%), Space: 45.9 MB (28%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elementsrecursive approach .java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) return null;
+        if(head.next == null && head.val == val) return null;
+
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        while(curr!=null){
+            if(curr.val==val){
+                prev.next = prev.next.next;
+            }else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
