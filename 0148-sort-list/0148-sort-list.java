af98383d483759d09adc778e4a729d982b440878
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-07-03 12:38:31 +0530

    Time: 9 ms (96.39%), Space: 56.8 MB (62.07%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    // Helper to find the middle node
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // Merge two sorted linked lists
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+
+        // Attach remaining nodes
+        if (head1 != null) temp.next = head1;
+        if (head2 != null) temp.next = head2;
+
+        return dummy.next;
+    }
+
+    // Recursive Merge Sort
+    private ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode mid = getMid(head);
+        ListNode rightHead = mid.next;
+        mid.next = null; // Split the list
+
+        ListNode left = mergeSort(head);
+        ListNode right = mergeSort(rightHead);
+
+        return merge(left, right);
+    }
+
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
+    }
+}
