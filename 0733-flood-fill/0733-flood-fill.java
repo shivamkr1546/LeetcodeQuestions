ccbe7fd9b9a7ee3a5ad828448340650f78ab264f
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-07-25 09:40:49 +0530

    Time: 0 ms (100%), Space: 45.1 MB (32.39%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,6 +1,6 @@
 class Solution {
     public void dfs(int[][] image, int i, int j, int r, int c, int originalColor, int newColor){
-        if(i < 0 || j < 0 || i >= r || j >= c || image[i][j] != originalColor)
+        if(i < 0 || j < 0 || i >= r || j >= c || image[i][j] != originalColor || image[i][j] == newColor)
             return;
 
         image[i][j] = newColor;
@@ -16,8 +16,6 @@ class Solution {
         int c = image[0].length;
 
         int originalColor = image[sr][sc];
-        if(originalColor == newColor) return image;
-
         dfs(image, sr, sc, r, c, originalColor, newColor);
         return image;
     }
