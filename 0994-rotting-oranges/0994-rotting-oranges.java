3b6536ac13eeb16c133daebb63d366c6fb22fae5
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-07-27 14:10:45 +0530

    Time: 2 ms (67.71%), Space: 42.3 MB (75.67%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,43 +1,52 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
 
-        Queue<int[]> queue = new LinkedList<>();
-        int fresh = 0;
+        Queue<int[]> q = new LinkedList<>();
 
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-                if(grid[i][j] == 2) {
-                    queue.offer(new int[]{i, j});
-                } else if(grid[i][j] == 1) {
-                    fresh++;
+        int[][] vis = new int[n][m];
+        int cntFresh = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i, j, 0});
+                    vis[i][j] = 2;
+                }else{
+                    vis[i][j] = 0;
                 }
+
+                if(grid[i][j] == 1) cntFresh++;
             }
         }
         
-        if(fresh == 0) return 0;
-        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        int min = -1;
 
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            min++;
-            for(int i=0; i<size; i++){
-                int[] pos = queue.poll();
-                for(int[] dir : directions){
-                    int x = pos[0] + dir[0];
-                    int y = pos[1] + dir[1];
+        int tm = 0;
+        int drow[] = {-1, 0, +1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        int cnt = 0;
 
-                    if(x >= 0 && y >= 0 && x < r && y < c && grid[x][y] == 1){
-                        grid[x][y] = 2;
-                        fresh--;
-                        queue.offer(new int[]{x, y});
-                    }
-                }
-            }
+        while(!q.isEmpty()){
+            int[] pos = q.poll();
+            int r = pos[0];
+            int c = pos[1];
+            int t = pos[2];
+
+            tm = Math.max(tm, t);
+
+            for(int i=0; i<4; i++){
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                    q.offer(new int[]{nrow, ncol, t+1});
+                    vis[nrow][ncol] = 2;
+                    cnt++;
+                } 
+            }           
         }
-        return fresh == 0 ? min : -1;
-        
+        if(cnt == cntFresh) return tm;
+        return -1;
     }
 }
