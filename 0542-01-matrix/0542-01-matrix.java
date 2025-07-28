bd599c8af80d250cea92de7117a47c8494bb3ff3
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-07-28 11:29:36 +0530

    Time: 12 ms (93.51%), Space: 47.6 MB (36.05%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,38 +1,38 @@
 class Solution {
-    public int[][] updateMatrix(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
 
-        Queue<int[]> queue = new LinkedList<>();
-        int zero = 0;
-
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-                if(grid[i][j] == 0) {
-                    queue.offer(new int[]{i, j});
-                } else {
-                    grid[i][j] = -1;
+        Queue<int[]> q = new LinkedList<>();
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 0){
+                    q.offer(new int[]{i,j});
+                }else{
+                    mat[i][j] = -1;
                 }
             }
         }
-        
-        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-
-        while(!queue.isEmpty()){
-            int size = queue.size();
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0 , -1};
+        while(!q.isEmpty()){
+            int size = q.size();
             for(int i=0; i<size; i++){
-                int[] pos = queue.poll();
-                for(int[] dir : directions){
-                    int x = pos[0] + dir[0];
-                    int y = pos[1] + dir[1];
-
-                    if(x >= 0 && y >= 0 && x < r && y < c && grid[x][y] == -1){
-                        grid[x][y] = grid[pos[0]][pos[1]] + 1;
-                        queue.offer(new int[]{x, y});
+                int[] pos = q.poll();
+                int r = pos[0];
+                int c = pos[1];
+                for(int j=0; j<4; j++){
+                    int nrow = r + drow[j];
+                    int ncol = c + dcol[j];
+                    if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && mat[nrow][ncol] == -1){
+                        mat[nrow][ncol] = mat[pos[0]][pos[1]] + 1;
+                        q.offer(new int[]{nrow, ncol});
                     }
                 }
             }
         }
-        return grid;
+
+        return mat;
     }
 }
