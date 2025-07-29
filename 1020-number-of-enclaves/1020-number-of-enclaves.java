6d68a83d84059e1f8592c259bc9e986d508014cf
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-07-29 20:53:37 +0530

    Time: 6 ms (98.69%), Space: 57.2 MB (46.82%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,42 +1,36 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
         int n = grid.length, m = grid[0].length;
-        int[][] vis = new int[n][m];
-        int[] dr = {-1, 0, 1, 0};
-        int[] dc = {0, 1, 0, -1};
 
-        // Step 1: DFS from border 1s
+        // DFS from border 1s, mark visited land as -1
         for (int i = 0; i < n; i++) {
-            if (grid[i][0] == 1 && vis[i][0] == 0)
-                dfs(i, 0, grid, vis, dr, dc);
-            if (grid[i][m - 1] == 1 && vis[i][m - 1] == 0)
-                dfs(i, m - 1, grid, vis, dr, dc);
+            if (grid[i][0] == 1) dfs(i, 0, grid);
+            if (grid[i][m - 1] == 1) dfs(i, m - 1, grid);
         }
 
         for (int j = 0; j < m; j++) {
-            if (grid[0][j] == 1 && vis[0][j] == 0)
-                dfs(0, j, grid, vis, dr, dc);
-            if (grid[n - 1][j] == 1 && vis[n - 1][j] == 0)
-                dfs(n - 1, j, grid, vis, dr, dc);
+            if (grid[0][j] == 1) dfs(0, j, grid);
+            if (grid[n - 1][j] == 1) dfs(n - 1, j, grid);
         }
 
-        // Step 2: Count all 1s not visited
+        // Count remaining land
         int count = 0;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                if (grid[i][j] == 1 && vis[i][j] == 0)
+                if (grid[i][j] == 1)
                     count++;
 
         return count;
     }
 
-    void dfs(int r, int c, int[][] grid, int[][] vis, int[] dr, int[] dc) {
-        vis[r][c] = 1;
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i], nc = c + dc[i];
-            if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length
-                && grid[nr][nc] == 1 && vis[nr][nc] == 0)
-                dfs(nr, nc, grid, vis, dr, dc);
-        }
+    void dfs(int r, int c, int[][] grid) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] != 1)
+            return;
+        grid[r][c] = -1;
+
+        dfs(r - 1, c, grid);
+        dfs(r + 1, c, grid);
+        dfs(r, c - 1, grid);
+        dfs(r, c + 1, grid);
     }
 }
