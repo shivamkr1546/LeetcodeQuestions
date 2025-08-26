bf69a69b901b9b4016401e068e09674fec9aee94
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-08-26 13:32:56 +0530

    Time: 5 ms (83.45%), Space: 44 MB (41.33%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,40 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        int total = 0;
-        for(int num : nums) total+=num;
+    public int findTargetSumWays(int[] arr, int d) {
+        int sum = 0;
+        
+        for (int ele : arr) {
+            sum += ele;
+        }
+        
+        if(Math.abs(d) > sum) return 0;
+        if((sum + d) % 2 != 0) return 0;
+        
+        int s1 = (sum+d)/2;
+    
+        int ways = subsetSum(arr, s1, arr.length);
+        return ways;
+    }
 
-        if (Math.abs(target) > total || (total + target) % 2 == 1) return 0;
-
-        int half = (total + target)/2;
-
-        int[] dp = new int[half + 1];
-        dp[0] = 1;
-
-        for(int num: nums){
-            for(int s = half; s>=num; s--){
-                dp[s] += dp[s-num];
+    int subsetSum(int[] arr, int sum, int n){
+        int[][] dp = new int[n+1][sum+1];
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int j=1; j<=sum; j++){
+            dp[0][j] = 0;
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<=sum; j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-
-        return dp[half];
-
+        
+        return dp[n][sum];
     }
 }
