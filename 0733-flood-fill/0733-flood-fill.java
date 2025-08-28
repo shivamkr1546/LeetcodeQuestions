af83f0a82860f64aac2f68b974625bc020bc8963
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-08-28 23:47:44 +0530

    Time: 0 ms (100%), Space: 44.9 MB (68.9%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,22 +1,22 @@
 class Solution {
-    public void dfs(int[][] image, int i, int j, int r, int c, int originalColor, int newColor){
-        if(i < 0 || j < 0 || i >= r || j >= c || image[i][j] != originalColor || image[i][j] == newColor)
-            return;
+    public void dfs(int[][] image, int sr, int sc,int oldColor, int newColor, int maxRow, int maxCol){
+        if(sr < 0 || sr >= maxRow || sc < 0 || sc >= maxCol || image[sr][sc]!=oldColor) return;
 
-        image[i][j] = newColor;
+        image[sr][sc] = newColor;
 
-        dfs(image, i + 1, j, r, c, originalColor, newColor);
-        dfs(image, i - 1, j, r, c, originalColor, newColor);
-        dfs(image, i, j + 1, r, c, originalColor, newColor);
-        dfs(image, i, j - 1, r, c, originalColor, newColor);
+        dfs(image, sr+1, sc,oldColor, newColor, maxRow, maxCol);
+        dfs(image, sr-1, sc,oldColor, newColor, maxRow, maxCol);
+        dfs(image, sr, sc+1,oldColor, newColor, maxRow, maxCol);
+        dfs(image, sr, sc-1,oldColor, newColor, maxRow, maxCol);
     }
 
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int r = image.length;
         int c = image[0].length;
+        int oldColor = image[sr][sc];
 
-        int originalColor = image[sr][sc];
-        dfs(image, sr, sc, r, c, originalColor, newColor);
+        if(oldColor == color) return image;
+        dfs(image, sr, sc,oldColor, color, r, c);
         return image;
     }
 }
