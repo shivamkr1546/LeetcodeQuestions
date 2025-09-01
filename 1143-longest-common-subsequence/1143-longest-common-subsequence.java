c938aee670ef35156a177770823ccbc3bfdf4ba5
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-09-01 13:32:41 +0530

    Time: 20 ms (73%), Space: 51.5 MB (7.05%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,6 +4,7 @@ class Solution {
         int m = text2.length();
 
         int[][] dp = new int[n+1][m+1];
+
         for(int i=0; i<=n; i++) dp[i][0] = 0;
         for(int j=1; j<=m; j++) dp[0][j] = 0;
 
@@ -12,7 +13,7 @@ class Solution {
                 if(text1.charAt(i-1) == text2.charAt(j-1)){
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]); 
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
                 }
             }
         }
