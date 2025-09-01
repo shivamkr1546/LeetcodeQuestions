15fddb118256dfa23e0188b9b4de0afa54b42a0f
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-09-01 10:38:27 +0530

    Time: 35 ms (13.98%), Space: 46.5 MB (5.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,16 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
+        int n = coins.length;
+        int[][] dp = new int[n+1][amount+1];
 
-        Arrays.fill(dp, amount+1);
-        dp[0]=0;
+        for(int i=0; i<=n; i++) Arrays.fill(dp[i], -1);
+        int ans = memo(coins, amount, n, dp);
+        return (ans == Integer.MAX_VALUE - 1 ? -1 : ans);
+    }
 
-        for(int coin: coins){
-            for(int i=coin; i<=amount; i++){
-                dp[i] = Math.min(dp[i], dp[i-coin]+1);
-            }
+    int memo(int[] coins, int sum, int n, int[][] dp){
+        if(sum == 0) return 0;
+        if(n==0) return Integer.MAX_VALUE - 1;
+
+        if(dp[n][sum] != -1) return dp[n][sum];
+
+        if(coins[n-1] <= sum){
+            return dp[n][sum] = Math.min(memo(coins, sum, n-1, dp), 1 + memo(coins, sum-coins[n-1], n, dp));
+        }else{
+            return dp[n][sum] = memo(coins, sum, n-1, dp);
         }
-
-        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
