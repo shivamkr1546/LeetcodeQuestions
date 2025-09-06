bfd7f32a94e25604abf6918f4df9f063ea971f7a
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-09-06 23:58:08 +0530

    Time: 11 ms (34.26%), Space: 49.4 MB (27.14%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,21 +2,19 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n+1][amount+1];
-        for(int i=0; i<=n; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        
+
+        for(int i=0; i<=n; i++) Arrays.fill(dp[i], -1);
         return memo(coins, amount, n, dp);
     }
 
     public int memo(int[] coins, int sum, int n, int[][] dp){
-        if(sum==0) return 1;
         if(n==0) return 0;
-        
+        if(sum==0) return 1;
+
         if(dp[n][sum] != -1) return dp[n][sum];
-        
+
         if(coins[n-1] <= sum){
-            return dp[n][sum] = memo(coins, sum, n-1, dp) + memo(coins, sum - coins[n-1], n, dp);
+            return dp[n][sum] = memo(coins, sum-coins[n-1], n, dp) + memo(coins, sum, n-1, dp);
         }else{
             return dp[n][sum] = memo(coins, sum, n-1, dp);
         }
