73181159174a186fc333c1d792fe8c6b768f601f
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-10-29 09:36:15 +0530

    Time: 0 ms (100%), Space: 40.5 MB (95.94%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
@@ -1,12 +1,6 @@
 class Solution {
     public int smallestNumber(int n) {
-        if(n==1) return 1;
-        for(int i=0; i<n; i++){
-            if((int)Math.pow(2, i) > n){
-                int res = (int)Math.pow(2, i);
-                return res - 1;
-            }
-        }
-        return -1;
+        int msb = Integer.highestOneBit(n);
+        return (msb<<1)-1;
     }
 }
