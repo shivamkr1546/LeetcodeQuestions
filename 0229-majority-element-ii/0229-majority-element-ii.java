01d24b1a377a0322d2cc111b07ffb375433c5bd5
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-11-01 12:16:10 +0530

    Time: 2 ms (99.8%), Space: 50.5 MB (28.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,13 +1,35 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> mp = new HashMap<>();
+        int can1 = 0, can2 = 0;
+        int lead1 = 0, lead2 = 0;
 
         for(int i=0; i<nums.length; i++){
-            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+            if(lead1 == 0 && can2 != nums[i]){
+                lead1++;
+                can1 = nums[i];
+            }
+            else if(lead2 == 0 && can1 != nums[i]){
+                lead2++;
+                can2 = nums[i];
+            }
+            else if(nums[i] == can1) lead1++;
+            else if(nums[i] == can2) lead2++;
+            else{
+                lead1--;
+                lead2--;
+            }
         }
+
         List<Integer> ans = new ArrayList<>();
-        for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
-            if(entry.getValue() > nums.length/3) ans.add(entry.getKey());
+        int cnt1 = 0, cnt2 = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == can1) cnt1++;
+            if(nums[i] == can2) cnt2++;
+        }
+        
+        if(cnt1 > nums.length/3) ans.add(can1);
+        if(cnt2 > nums.length/3){
+            if(!ans.contains(can2)) ans.add(can2);
         }
         return ans;
     }
