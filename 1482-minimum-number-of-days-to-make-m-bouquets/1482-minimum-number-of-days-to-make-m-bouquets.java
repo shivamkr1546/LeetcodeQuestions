632957a62e8fcad9d78a361e90808075335328bb
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-11-10 11:04:09 +0530

    Time: 19 ms (54.49%), Space: 82.6 MB (6.7%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,41 +1,39 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
+        int l = Integer.MAX_VALUE;
+        int h = Integer.MIN_VALUE;
         for(int d : bloomDay){
-            min = Math.min(min, d);
-            max = Math.max(max, d);
-        }
-
-        return binarySearch(bloomDay, m, k, min, max);
+            l = Math.min(l , d);
+            h = Math.max(h , d);
+        }        
+        return binarySearch(bloomDay, m, k, l, h);
     }
 
-    public int binarySearch(int[] bloomDay, int m, int k, int min, int max){
-        int minDays = -1;
-        while(min <= max){
-            int days = min + (max - min)/2;
-            if(check(bloomDay, m, k, days) >= m){
-                minDays = days;
-                max = days - 1;
+    public int binarySearch(int[] bloomDay, int m, int k, int l, int h){
+        int ans = -1;
+        while(l<=h){
+            int days = l + (h - l)/2;
+            if(check(bloomDay, days, m, k) >= m){
+                ans = days;
+                h = days - 1;
             }else{
-                min = days + 1;
+                l = days + 1;
             }
         }
-        return minDays;
+        return ans;
     }
 
-    public int check(int[] bloomDay, int m, int k, int day){
+    public int check(int[] bloomDay, int days, int m, int k){
         int cnt = 0;
         int b = 0;
         for(int i=0; i<bloomDay.length; i++){
-            if(bloomDay[i] <= day){
+            if(bloomDay[i] <= days){
                 cnt++;
+                if(cnt == k){
+                    b++;
+                    cnt = 0;
+                }
             }else{
-                cnt=0;
-            }
-            if(cnt == k){
-                b++;
                 cnt = 0;
             }
         }
