20e715b3304ec8109dfd66450197756b9612258d
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-11-27 00:57:12 +0530

    Time: 4 ms (99.72%), Space: 47.4 MB (38.11%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,11 +1,11 @@
-class Solution {
+public class Solution {
     public boolean validPalindrome(String s) {
-        int l = 0;
-        int r = s.length()-1;
+        int l = 0, r = s.length() - 1;
 
-        while(l<r){
-            if(s.charAt(l) != s.charAt(r)){
-                return isPalin(s.substring(0,l) + s.substring(l+1)) || isPalin(s.substring(0,r) + s.substring(r+1));
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return isPalindrome(s, l + 1, r) ||
+                       isPalindrome(s, l, r - 1);
             }
             l++;
             r--;
@@ -14,10 +14,9 @@ class Solution {
         return true;
     }
 
-    private boolean isPalin(String s){
-        int l = 0, r = s.length() - 1;
-        while(l<r){
-            if(s.charAt(l) != s.charAt(r)){
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
                 return false;
             }
             l++;
