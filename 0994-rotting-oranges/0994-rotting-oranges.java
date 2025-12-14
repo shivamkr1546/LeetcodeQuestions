379ac8041557ae5af06da44249b676ebab84bd51
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-12-14 19:04:54 +0530

    Time: 2 ms (83.4%), Space: 44.1 MB (38.64%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,52 +1,45 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        int r = grid.length, c = grid[0].length;
 
         Queue<int[]> q = new LinkedList<>();
-
-        int[][] vis = new int[n][m];
+        int[][] vis = new int[r][c];
         int cntFresh = 0;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
                 if(grid[i][j] == 2){
                     q.offer(new int[]{i, j, 0});
                     vis[i][j] = 2;
-                }else{
+                }
+                if(grid[i][j] == 1){
+                    cntFresh++;
                     vis[i][j] = 0;
                 }
-
-                if(grid[i][j] == 1) cntFresh++;
             }
         }
-        
 
-        int tm = 0;
-        int drow[] = {-1, 0, +1, 0};
-        int dcol[] = {0, 1, 0, -1};
-        int cnt = 0;
+        int tm = 0, cnt = 0;
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1};
 
         while(!q.isEmpty()){
             int[] pos = q.poll();
-            int r = pos[0];
-            int c = pos[1];
-            int t = pos[2];
-
+            int row = pos[0], col = pos[1], t = pos[2];
             tm = Math.max(tm, t);
 
             for(int i=0; i<4; i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
+                int nRow = row + dRow[i];
+                int nCol = col + dCol[i];
 
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
-                    q.offer(new int[]{nrow, ncol, t+1});
-                    vis[nrow][ncol] = 2;
+                if(nRow >= 0 && nCol >= 0&& nRow < r && nCol < c && grid[nRow][nCol] == 1 && vis[nRow][nCol] == 0){
+                    q.offer(new int[]{nRow, nCol, t+1});
+                    vis[nRow][nCol] = 2;
                     cnt++;
-                } 
-            }           
+                }
+            }
         }
-        if(cnt == cntFresh) return tm;
-        return -1;
+
+        return (cnt == cntFresh) ? tm : -1;
     }
 }
