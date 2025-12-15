73dd4b06d5cfe1203651fc072513735cc26b6e92
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-12-15 17:23:15 +0530

    Time: 47 ms (54.93%), Space: 185.3 MB (20.04%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.java
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -1,38 +1,42 @@
 class Solution {
     public int[][] highestPeak(int[][] isWater) {
-        int r = isWater.length;
-        int c = isWater[0].length;
+        int n = isWater.length;
+        int m = isWater[0].length;
 
-        Queue<int[]> queue = new LinkedList<>();
+        int[][] dist = new int[n][m];
+        boolean[][] vis = new boolean[n][m];
 
-        // Step 1: Initialize water cells as 0 and land as -1
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-                if(isWater[i][j] == 1) {
-                    isWater[i][j] = 0;  // Water height is 0
-                    queue.offer(new int[]{i, j});
-                } else {
-                    isWater[i][j] = -1; // Mark unvisited land
-                }
-            }
-        }
-        
-        int[][] directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        Queue<int[]> q = new LinkedList<>();
 
-        // Step 2: BFS to assign heights
-        while(!queue.isEmpty()){
-            int[] pos = queue.poll();
-            for(int[] dir : directions){
-                int x = pos[0] + dir[0];
-                int y = pos[1] + dir[1];
-
-                if(x >= 0 && y >= 0 && x < r && y < c && isWater[x][y] == -1){
-                    isWater[x][y] = isWater[pos[0]][pos[1]] + 1;
-                    queue.offer(new int[]{x, y});
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(isWater[i][j] == 1){
+                    vis[i][j] = true;
+                    q.offer(new int[]{i, j});
                 }
             }
         }
 
-        return isWater;
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1};
+
+        while(!q.isEmpty()){
+            int[] pos = q.poll();
+            int row = pos[0];
+            int col = pos[1];
+
+            for(int i=0; i<4; i++){
+                int nRow = row + dRow[i];
+                int nCol = col + dCol[i];
+
+                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol]){
+                    vis[nRow][nCol] = true;
+                    dist[nRow][nCol] = dist[row][col] + 1;
+                    q.offer(new int[]{nRow, nCol});
+                }
+            }
+        }
+
+        return dist;
     }
 }
