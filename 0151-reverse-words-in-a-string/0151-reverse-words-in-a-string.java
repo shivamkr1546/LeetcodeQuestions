afe5a8af534ad4d99aa54791e292029cd796d620
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-12-24 21:37:19 +0530

    Time: 7 ms (66.14%), Space: 43.8 MB (85.57%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,17 +1,72 @@
 class Solution {
     public String reverseWords(String s) {
-        String trim = s.trim();
-        String[] arr = trim.split("\\s+");
+        // trim
+        // l, r, s
+        // sb -> s cleaned with multiple spaces to a single space
+        // reverse sb
+        // reverse word by word in sb
+
+        int l = 0;
+        int r = s.length() - 1;
+
+        while(l<s.length()){
+            if(s.charAt(l) == ' ') l++;
+            else break;
+        }
+
+        while(r >= 0){
+            if(s.charAt(r) == ' ') r--;
+            else break;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(l<=r){
+            if(s.charAt(l)!=' '){
+                sb.append(s.charAt(l));
+                l++;
+            }else if(s.charAt(l) == ' '){
+                if(sb.charAt(sb.length() - 1) != ' '){
+                    sb.append(' ');
+                    l++;
+                }else{
+                    l++;
+                }
+            }
+        }
 
         int i = 0;
-        int j = arr.length - 1;
+        int j = sb.length() - 1;
+
         while(i<j){
-            String temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            char temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(j));
+            sb.setCharAt(j, temp);
             i++;
             j--;
         }
-        return String.join(" ", arr);
+
+        int st=0;
+        int en=0;
+        while(st<sb.length()){
+            while(en<sb.length() && sb.charAt(en)!=' '){
+                en++;
+            }
+
+            int p1 = st;
+            int p2 = en - 1;
+            while(p1<p2){
+                char temp = sb.charAt(p1);
+                sb.setCharAt(p1, sb.charAt(p2));
+                sb.setCharAt(p2, temp);
+                p1++;
+                p2--;
+            }
+
+            st = en + 1;
+            en = st;
+        }
+
+        return sb.toString();
     }
 }
