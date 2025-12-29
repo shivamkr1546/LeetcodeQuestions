d1cf95aee7525b2bb4710b93b7f12ceb2f06f32f
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-12-29 20:01:07 +0530

    Time: 1 ms (95.24%), Space: 47.4 MB (17.36%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -1,46 +1,17 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        //Iterative Approach
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
-        // if(head == null) return null;
-        // if(head.next == null && head.val == val) return null;
-
-        // ListNode dummy = new ListNode();
-        // dummy.next = head;
-        // ListNode prev = dummy;
-        // ListNode curr = head;
-
-        // while(curr!=null){
-        //     if(curr.val==val){
-        //         prev.next = prev.next.next;
-        //     }else{
-        //         prev = curr;
-        //     }
-        //     curr = curr.next;
-        // }
-
-        // return dummy.next;
-
-        //recursive approach
-        if(head == null){
-            return null;
+        ListNode curr = dummy;
+        while (curr.next != null) {
+            if (curr.next.val == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
         }
 
-        head.next = removeElements(head.next, val);
-
-        if(head.val == val){
-            return head.next;
-        }
-        return head;
+        return dummy.next;
     }
 }
