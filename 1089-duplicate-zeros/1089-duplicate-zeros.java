05e036254727d6ca1d4004560792cfc058d02dde
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2025-12-29 08:24:34 +0530

    Time: 1 ms (98.05%), Space: 47.1 MB (6.58%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,27 +1,28 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int zCnt = 0;
-        int n = arr.length - 1;
+        int pzd = 0;
+        int lastIdx = arr.length - 1;
 
-        for(int i=0; i<=n - zCnt; i++){
-            if(arr[i]==0){
-                if(i == n - zCnt){
-                    arr[n] = 0;
-                    n -= 1;
+        for(int i=0; i<=lastIdx - pzd; i++){
+            if(arr[i] == 0){
+                if(i == lastIdx - pzd){
+                    arr[lastIdx] = 0;
+                    lastIdx -= 1;
                     break;
                 }
-                zCnt++;
+                pzd++;
             }
         }
 
-        int newLastIdx = n - zCnt;
+
+        int newLastIdx = lastIdx - pzd;
         for(int i=newLastIdx; i>=0; i--){
             if(arr[i] == 0){
-                arr[i + zCnt] = 0;
-                zCnt--;
-                arr[i + zCnt] = 0;
+                arr[i+pzd] = 0;
+                pzd--;
+                arr[i+pzd] = 0;
             }else{
-                arr[i + zCnt] = arr[i];
+                arr[i+pzd] = arr[i];
             }
         }
     }
