b0703d22d6de5076fc8462c49c4c9a004efc09c1
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-01-04 00:24:41 +0530

    Time: 1 ms (99.95%), Space: 44.5 MB (95.88%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String reversePrefix(String s, int k) {
+        char[] arr = s.toCharArray();
+        int i = 0;
+        int j = k-1;
+        while(i<j){
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j--;
+            i++;
+        }
+
+        return new String(arr);
+    }
+}
