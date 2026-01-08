216d462ff446468b0cc24bf7c6f4f5c9d84fe263
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-01-08 20:35:51 +0530

    Time: 0 ms (100%), Space: 48.2 MB (31.65%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,50 @@
 class Solution {
-    private static int firstOccurance(int[] nums, int target){
-        int l = 0;
-        int h = nums.length - 1;
-        int ans = -1;
-        while(l<=h){
-            int mid = l + (h - l)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                h = mid - 1;
-            }
-            else if(nums[mid] > target){
-                h = mid - 1;
-            }else{
-                l = mid + 1;
-            }
-        }
-        return ans;
-    }
-
-    private static int lastOccurance(int[] nums, int target){
-        int l = 0;
-        int h = nums.length - 1;
-        int ans = -1;
-        while(l<=h){
-            int mid = l + (h - l)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                l = mid + 1;
-            }
-            else if(nums[mid] > target){
-                h = mid - 1;
-            }else{
-                l = mid + 1;
-            }
-        }
-        return ans;
-    }
-
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1,-1};
-        result[0] = firstOccurance(nums, target);
-        result[1] = lastOccurance(nums, target);
+        int[] result = {-1, -1};
+        if (nums.length == 0) return result;
+
+        int first = firstOccur(nums, target);
+        
+        if (first == nums.length || nums[first] != target) {
+            return result;
+        }
+
+        int last = lastOccur(nums, target);
+
+        result[0] = first;
+        result[1] = last;
         return result;
+    }
+
+    public int firstOccur(int[] nums, int target){
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while(lo<=hi){
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] >= target){
+                hi = mid - 1;
+            }else{
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
+    public int lastOccur(int[] nums, int target){
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while(lo<=hi){
+            int mid = lo + (hi - lo)/2;
+            if(nums[mid] > target){
+                hi = mid - 1;
+            }else{
+                lo = mid + 1;
+            }
+        }
+
+        return lo - 1;
     }
 }
