706244b687d40f3a038b8840778c27acf556417d
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-01-09 08:07:05 +0530

    Time: 9 ms (98.6%), Space: 59.5 MB (44.54%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,15 +9,8 @@
  * }
  */
 class Solution {
-    public ListNode findMid(ListNode head){
-        ListNode slow = head;
-        ListNode fast = head.next;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        return slow;
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
     }
 
     public ListNode merge(ListNode head1, ListNode head2){
@@ -42,7 +35,7 @@ class Solution {
     }
 
     public ListNode mergeSort(ListNode head){
-        if(head == null || head.next == null) return head;
+        if(head==null || head.next==null) return head;
 
         ListNode mid = findMid(head);
         ListNode rHead = mid.next;
@@ -54,7 +47,14 @@ class Solution {
         return merge(left, right);
     }
 
-    public ListNode sortList(ListNode head) {
-        return mergeSort(head);
+    public ListNode findMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
     }
 }
