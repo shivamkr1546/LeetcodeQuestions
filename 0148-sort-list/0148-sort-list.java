ad2bc94e7f13d339ef50d8c698d503084c00418c
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-01-09 06:24:39 +0530

    Time: 9 ms (98.6%), Space: 59.4 MB (58.71%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,52 +9,47 @@
  * }
  */
 class Solution {
-
-    // Helper to find the middle node
-    private ListNode getMid(ListNode head) {
+    public ListNode findMid(ListNode head){
         ListNode slow = head;
         ListNode fast = head.next;
-
-        while (fast != null && fast.next != null) {
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
+
         return slow;
     }
 
-    // Merge two sorted linked lists
-    private ListNode merge(ListNode head1, ListNode head2) {
+    public ListNode merge(ListNode head1, ListNode head2){
         ListNode dummy = new ListNode(-1);
         ListNode temp = dummy;
 
-        while (head1 != null && head2 != null) {
-            if (head1.val <= head2.val) {
+        while(head1 != null && head2 != null){
+            if(head1.val <= head2.val){
                 temp.next = head1;
                 head1 = head1.next;
-            } else {
+            }else{
                 temp.next = head2;
                 head2 = head2.next;
             }
             temp = temp.next;
         }
 
-        // Attach remaining nodes
-        if (head1 != null) temp.next = head1;
-        if (head2 != null) temp.next = head2;
+        if(head1 != null) temp.next = head1;
+        if(head2 != null) temp.next = head2;
 
         return dummy.next;
     }
 
-    // Recursive Merge Sort
-    private ListNode mergeSort(ListNode head) {
-        if (head == null || head.next == null) return head;
+    public ListNode mergeSort(ListNode head){
+        if(head == null || head.next == null) return head;
 
-        ListNode mid = getMid(head);
-        ListNode rightHead = mid.next;
-        mid.next = null; // Split the list
+        ListNode mid = findMid(head);
+        ListNode rHead = mid.next;
+        mid.next = null;
 
         ListNode left = mergeSort(head);
-        ListNode right = mergeSort(rightHead);
+        ListNode right = mergeSort(rHead);
 
         return merge(left, right);
     }
