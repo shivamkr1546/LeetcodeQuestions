1bd421f8edbc8147310214035d2ac97cc82b39c6
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-01-24 23:03:59 +0530

    Time: 10 ms (53.34%), Space: 59.5 MB (39.45%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -25,35 +25,36 @@ class Solution {
         return merge(left, right);
     }
 
-    public ListNode merge(ListNode head1, ListNode head2){
-        ListNode dummy = new ListNode(0);
-        ListNode temp = dummy;
-
-        while(head1!=null && head2!=null){
-            if(head1.val <= head2.val){
-                temp.next = head1;
-                head1 = head1.next;
-            }else{
-                temp.next = head2;
-                head2 = head2.next;
-            }
-            temp = temp.next;
-        }
-
-        if(head1!=null) temp.next = head1;
-        if(head2!=null) temp.next = head2;
-
-        return dummy.next;
-    }
-
     public ListNode getMid(ListNode head){
         ListNode slow = head;
         ListNode fast = head.next;
-        while(fast != null && fast.next != null){
+
+        while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
         }
 
         return slow;
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2){
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while(head1!=null && head2!=null){
+            if(head1.val <= head2.val){
+                curr.next = head1;
+                head1 = head1.next;
+            }else{
+                curr.next = head2;
+                head2 = head2.next;
+            }
+            curr = curr.next;
+        }
+
+        if(head1!=null) curr.next = head1;
+        if(head2!=null) curr.next = head2;
+
+        return dummy.next;
     }
 }
