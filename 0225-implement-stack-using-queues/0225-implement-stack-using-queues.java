46f09b363885edc86c05a5954c7aa884a28dae3d
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-02-09 10:01:48 +0530

    Time: 0 ms (100%), Space: 42.8 MB (58.01%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,69 +1,26 @@
 class MyStack {
-    Queue<Integer> q1;
-    Queue<Integer> q2;
-
-    // Constructor
+    Queue<Integer> q;
     public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+        q = new LinkedList<>();
     }
     
     public void push(int x) {
-        if(!q1.isEmpty()){
-            q1.add(x);
-        }else{
-            q2.add(x);
+        q.add(x);
+        for(int i=1; i<q.size(); i++){
+            q.add(q.remove());
         }
     }
     
     public int pop() {
-        if(empty()){
-            return -1;
-        }
-        int head = -1;
-        //case 1
-        if(!q1.isEmpty()){
-            while(!q1.isEmpty()){
-                head = q1.remove();
-                if(q1.isEmpty()){
-                    break;
-                }
-                q2.add(head);
-            }
-        }else{
-            while(!q2.isEmpty()){
-                head = q2.remove();
-                if(q2.isEmpty()){
-                    break;
-                }
-                q1.add(head);
-            }
-        }
-        return head;
+        return q.remove();
     }
     
     public int top() {
-        if(empty()){
-            return -1;
-        }
-        int top = -1;
-        //case 1
-        if(!q1.isEmpty()){
-            while(!q1.isEmpty()){
-                top = q1.remove();
-                q2.add(top);
-            }
-        }else{
-            while(!q2.isEmpty()){
-                top = q2.remove();
-                q1.add(top);
-            }
-        }
-        return top;
+        return q.peek();
     }
     
     public boolean empty() {
-        return q1.isEmpty() && q2.isEmpty();
+        return q.isEmpty();
     }
 }
 
