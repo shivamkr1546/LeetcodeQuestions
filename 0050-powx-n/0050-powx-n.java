5a00991b63d68d14b72b6e4ea8e8cc68f424b9d4
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-02-17 19:42:28 +0530

    Time: 0 ms (100%), Space: 48.1 MB (47.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,13 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        return pow(x, (long) n);
-    }
-
-    private double pow(double x, long n) {
-        if (n == 0) return 1;
-        if (n < 0) return 1 / pow(x, -n);
-        double half = pow(x, n / 2);
-        if (n % 2 == 0) return half * half;
-        return half * half * x;
+        
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
+        
+        double pow = 1;
+        
+        while(n != 0){
+            if((n & 1) != 0){
+                pow *= x;
+            } 
+                
+            x *= x;
+            n >>>= 1;
+            
+        }
+        
+        return pow;
     }
 }
