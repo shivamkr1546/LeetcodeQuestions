1ba978c7191039a0bcc33b8b6adae3808832a928
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-02-24 14:22:15 +0530

    Time: 10 ms (54.24%), Space: 59.6 MB (44.65%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,6 +9,18 @@
  * }
  */
 class Solution {
+
+    public ListNode getMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null) return head;
 
@@ -22,36 +34,25 @@ class Solution {
         return merge(left, right);
     }
 
-
-    public ListNode merge(ListNode head1, ListNode head2){
+    
+    public ListNode merge(ListNode h1, ListNode h2){
         ListNode dummy = new ListNode(-1);
         ListNode temp = dummy;
 
-        while(head1!=null && head2!=null){
-            if(head1.val < head2.val){
-                temp.next = head1;
-                head1 = head1.next;
+        while(h1!=null && h2!=null){
+            if(h1.val < h2.val){
+                temp.next = h1;
+                h1 = h1.next;
             }else{
-                temp.next = head2;
-                head2 = head2.next;
+                temp.next = h2;
+                h2 = h2.next;
             }
             temp = temp.next;
         }
 
-        if(head1!=null) temp.next = head1;
-        if(head2!=null) temp.next = head2;
+        if(h1!=null) temp.next = h1;
+        if(h2!=null) temp.next = h2;
+
         return dummy.next;
-    }
-
-    public ListNode getMid(ListNode head){
-        ListNode slow = head;
-        ListNode fast = head.next;
-
-        while(fast!=null && fast.next!=null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        return slow;
     }
 }
