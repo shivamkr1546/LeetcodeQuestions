e882c70b98609e11fdcb4b122b20eeb831694965
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-02-24 09:34:45 +0530

    Time: 10 ms (54.24%), Space: 59.5 MB (58.14%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -18,29 +18,31 @@ class Solution {
 
         ListNode left = sortList(head);
         ListNode right = sortList(rHead);
+
         return merge(left, right);
     }
 
+
     public ListNode merge(ListNode head1, ListNode head2){
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
         while(head1!=null && head2!=null){
             if(head1.val < head2.val){
-                curr.next = head1;
+                temp.next = head1;
                 head1 = head1.next;
             }else{
-                curr.next = head2;
+                temp.next = head2;
                 head2 = head2.next;
             }
-            curr = curr.next;
+            temp = temp.next;
         }
-        
-        if(head1!=null) curr.next = head1;
-        if(head2!=null) curr.next = head2;
-        
+
+        if(head1!=null) temp.next = head1;
+        if(head2!=null) temp.next = head2;
         return dummy.next;
     }
+
     public ListNode getMid(ListNode head){
         ListNode slow = head;
         ListNode fast = head.next;
