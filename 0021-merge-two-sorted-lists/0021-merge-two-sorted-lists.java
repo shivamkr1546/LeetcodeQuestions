ab57178391ee1fc38b698404f5d935572d9d972f
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-02-25 12:03:44 +0530

    Time: 0 ms (100%), Space: 44.5 MB (19.62%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,59 +9,19 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // if (list1 == null) return list2;
-        // if (list2 == null) return list1;
+    public ListNode helper(ListNode list1, ListNode list2){
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
 
-        // ListNode ptr;
-
-        // if (list1.val > list2.val) {
-        //     ptr = list2;
-        //     list2 = list2.next;
-        // } else {
-        //     ptr = list1;
-        //     list1 = list1.next;
-        // }
-
-        // ListNode curr = ptr;
-        
-        // while (list1 != null && list2 != null) {
-        //     if (list1.val < list2.val) {
-        //         curr.next = list1;
-        //         list1 = list1.next;
-        //     } else {
-        //         curr.next = list2;
-        //         list2 = list2.next;
-        //     }
-        //     curr = curr.next;
-        // }
-
-
-        // if (list1 == null)
-        //     curr.next = list2;
-        // else
-        //     curr.next = list1;
-
-        // return ptr;
-
-        //Approach 2;
-
-        ListNode c = new ListNode(0);
-        ListNode temp = c;
-        while(list1!=null && list2!=null){
-            if(list1.val <= list2.val){
-                temp.next = list1;
-                list1 = list1.next;
-                temp = temp.next;
-            }else{
-                temp.next = list2;
-                list2 = list2.next;
-                temp = temp.next;
-            }
+        if(list1.val <= list2.val){
+            list1.next = helper(list1.next, list2);
+            return list1;
+        }else{
+            list2.next = helper(list1, list2.next);
+            return list2;
         }
-
-        if(list1==null) temp.next = list2;
-        else temp.next = list1;
-        return c.next;
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return helper(list1, list2);
     }
 }
