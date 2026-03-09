e9a4b81fe3ce9c5e1ee1ed110c4b333927625047
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-03-09 08:05:06 +0530

    Time: 137 ms (29.54%), Space: 46.4 MB (27%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.java
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.java
@@ -1,11 +1,41 @@
 class Solution {
     public int punishmentNumber(int n) {
-        int[] arr = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909, 918,945,964,990,991,999,1000};
-        int sum = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<=n) sum += arr[i]*arr[i];
-            else if(arr[i]>n) break;
+        int punish = 0;
+
+        for(int num = 1; num <= n; num++){
+            int sq = num * num;
+
+            String s = String.valueOf(sq);
+            if(check(s, num, 0, 0)){
+                punish += sq;
+            }
         }
-        return sum;
+
+        return punish;
+    }
+
+    public boolean check(String str, int num, int i, int currSum){
+        if(i == str.length()){
+            return currSum == num;
+        }
+
+        if(currSum > num){
+            return false;
+        }
+
+        boolean possible = false;
+
+        for(int j = i; j < str.length(); j++){
+            String sub = str.substring(i, j + 1);
+            int val = Integer.parseInt(sub);
+
+            possible = possible || check(str, num, j + 1, currSum + val);
+
+            if(possible){
+                return true;
+            }
+        }
+
+        return possible;
     }
 }
