dc58e2ce8dac0f5ef27bde401e19f9bdc54eac76
shivamkr1546 <131629722+shivamkr1546@users.noreply.github.com>
2026-03-13 22:38:13 +0530

    Time: 623 ms (23.41%), Space: 43.2 MB (66.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,39 +1,18 @@
 class Solution {
-    public int findTargetSumWays(int[] arr, int d) {
-        int sum = 0;
-        
-        for (int ele : arr) {
-            sum += ele;
-        }
-
-        if((Math.abs(d) > sum) || (sum + d) % 2 != 0) return 0;
-        
-        int s1 = (sum+d)/2;
-    
-        int ways = subsetSum(arr, s1, arr.length);
-        return ways;
+    int count=0;
+    public int findTargetSumWays(int[] nums, int target) {
+        findCount(nums, target, 0, 0);
+        return count;
     }
 
-    int subsetSum(int[] arr, int sum, int n){
-        int[][] dp = new int[n+1][sum+1];
-        for(int i=0; i<=n; i++){
-            dp[i][0] = 1;
-        }
-        
-        for(int j=1; j<=sum; j++){
-            dp[0][j] = 0;
-        }
-        
-        for(int i=1; i<=n; i++){
-            for(int j=0; j<=sum; j++){
-                if(arr[i-1] <= j){
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
+    public void findCount(int[] nums, int target, int sum, int index) {
+        if(index==nums.length) {
+            if(sum==target) {
+                count++;
             }
+            return;
         }
-        
-        return dp[n][sum];
+        findCount(nums, target, sum+nums[index], index+1);
+        findCount(nums, target, sum-nums[index], index+1);
     }
 }
